5780f2da8c33b2b927bdcaf9b436c6501e7ce2ba
hungify <nmhungify@gmail.com>
2022-12-11 01:48:16 +0700

    fix(button): update variant and style

--- src/components/ButtonBase.vue
+++ src/components/ButtonBase.vue
@@ -1,10 +1,9 @@
 <script lang="ts" setup>
 import { computed, toRefs, useSlots } from 'vue';
-import { ObjectKeys } from '~/utils/object';
 
 interface ButtonProps {
   color?: 'default' | 'success' | 'info' | 'warning' | 'danger' | 'unstyled';
-  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'unstyled';
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
   variant?: 'outline' | 'solid' | 'ghost' | 'link';
   rounded?: boolean;
   disabled?: boolean;
@@ -49,12 +48,11 @@ const classes = computed(() => {
   const { color, size, rounded, disabled, loading, variant } = props;
   return {
     btn: true,
-    [`btn-${color}`]: color,
     [`btn-${size}`]: size,
     'btn-rounded': rounded,
     'btn-disabled': disabled,
     'btn-loading': loading,
-    [`btn-${variant}`]: variant,
+    [`btn-${variant}-${color}`]: color !== 'unstyled',
     'btn-expanded': props.expanded,
   };
 });
@@ -85,34 +83,6 @@ const slots = useSlots();
 </template>
 
 <style lang="scss" scoped>
-$btnColors: (
-  default: (
-    default: $default,
-    hover: $default-dark,
-    active: $default-light,
-  ),
-  info: (
-    default: $info,
-    hover: $info-dark,
-    active: $info-light,
-  ),
-  warning: (
-    default: $warning,
-    hover: $warning-dark,
-    active: $warning-light,
-  ),
-  success: (
-    default: $success,
-    hover: $success-dark,
-    active: $success-light,
-  ),
-  danger: (
-    default: $danger,
-    hover: $danger-dark,
-    active: $danger-light,
-  ),
-);
-
 $btnSizes: (
   xs: (
     padding: $xs-btn-padding,
@@ -141,6 +111,102 @@ $btnSizes: (
   ),
 );
 
+$outlineHoverOpacity: 0.2;
+$outlineActiveOpacity: 0.8;
+
+$solidHoverOpacity: 0.8;
+$solidActiveOpacity: 1;
+
+$ghostHoverOpacity: 0.1;
+$ghostActiveOpacity: 0.2;
+
+$btnVariants: (
+  outline: (
+    danger: (
+      default: $danger,
+      hover: rgba($danger-light, $outlineHoverOpacity),
+      active: rgba($danger-dark, $outlineActiveOpacity),
+    ),
+    success: (
+      default: $success,
+      hover: rgba($success-light, $outlineHoverOpacity),
+      active: rgba($success-dark, $outlineActiveOpacity),
+    ),
+    warning: (
+      default: $warning,
+      hover: rgba($warning-light, $outlineHoverOpacity),
+      active: rgba($warning-dark, $outlineActiveOpacity),
+    ),
+    info: (
+      default: $info,
+      hover: rgba($info-light, $outlineHoverOpacity),
+      active: rgba($info-dark, $outlineActiveOpacity),
+    ),
+  ),
+  solid: (
+    danger: (
+      default: $danger,
+      hover: rgba($danger, $solidHoverOpacity),
+      active: rgba($danger, $solidActiveOpacity),
+    ),
+    success: (
+      default: $success,
+      hover: $success-dark,
+      active: $success-light,
+    ),
+    warning: (
+      default: $warning,
+      hover: $warning-dark,
+      active: $warning-light,
+    ),
+    info: (
+      default: $info,
+      hover: $info-dark,
+      active: $info-light,
+    ),
+  ),
+  ghost: (
+    danger: (
+      default: $danger,
+      hover: rgba($danger, $ghostHoverOpacity),
+      active: rgba($danger, $ghostActiveOpacity),
+    ),
+    success: (
+      default: $success,
+      hover: rgba($success, $ghostHoverOpacity),
+      active: rgba($success, $ghostActiveOpacity),
+    ),
+    warning: (
+      default: $warning,
+      hover: rgba($warning, $ghostHoverOpacity),
+      active: rgba($warning, $ghostActiveOpacity),
+    ),
+    info: (
+      default: $info,
+      hover: rgba($info, $ghostHoverOpacity),
+      active: rgba($info, $ghostActiveOpacity),
+    ),
+  ),
+  link: (
+    danger: (
+      default: $danger,
+      active: rgba($danger-dark, 1),
+    ),
+    success: (
+      default: $success,
+      active: rgba($success-dark, 1),
+    ),
+    warning: (
+      default: $warning,
+      active: rgba($warning-dark, 1),
+    ),
+    info: (
+      default: $info,
+      active: rgba($info-dark, 1),
+    ),
+  ),
+);
+
 .btn {
   display: inline-flex;
   align-items: center;
@@ -150,7 +216,7 @@ $btnSizes: (
   transition: all 0.2s ease-in-out;
   cursor: pointer;
   font-family: $font-family;
-  color: rgba(22, 24, 35, 0.9);
+  background-color: transparent;
 
   & + & {
     margin-left: 1.2rem;
@@ -172,14 +238,6 @@ $btnSizes: (
   }
 }
 
-@each $size, $styles in $btnSizes {
-  .btn-#{$size} {
-    padding: map-get($styles, 'padding');
-    font-size: map-get($styles, 'font-size');
-    border-radius: map-get($styles, 'border-radius');
-  }
-}
-
 .btn-rounded {
   border-radius: 9999px;
 }
@@ -191,81 +249,71 @@ $btnSizes: (
 .btn-disabled {
   pointer-events: none;
   cursor: not-allowed;
-  color: rgba(22, 24, 35, 0.34);
-  background-color: rgba(22, 24, 35, 0.06);
+  color: rgba($text, 0.34);
+  background-color: rgba($text, 0.06);
 }
 
 .btn-loading {
-  opacity: 0.5;
+  opacity: 0;
   pointer-events: none;
 }
 
-@each $type, $colors in $btnColors {
-  .btn-#{$type} {
-    &.btn-ghost {
-      color: map-get($colors, 'default');
-      background-color: transparent;
+@each $size, $styles in $btnSizes {
+  .btn-#{$size} {
+    padding: map-get($styles, 'padding');
+    font-size: map-get($styles, 'font-size');
+    border-radius: map-get($styles, 'border-radius');
+  }
+}
 
-      &:hover {
-        color: map-get($colors, 'hover');
-        background-color: rgba(map-get($colors, 'hover'), 0.1);
+@each $variant in map-keys($btnVariants) {
+  @each $color in map-keys(map-get($btnVariants, $variant)) {
+    .btn-#{$variant}-#{$color} {
+      @if $variant == 'outline' {
+        color: map-get(map-get(map-get($btnVariants, $variant), $color), 'default');
+        border: 1px solid map-get(map-get(map-get($btnVariants, $variant), $color), 'default');
+
+        &:hover {
+          background-color: map-get(map-get(map-get($btnVariants, $variant), $color), 'hover');
+        }
+
+        &:active {
+          border-color: map-get(map-get(map-get($btnVariants, $variant), $color), 'active');
+        }
       }
-
-      &:active {
-        color: map-get($colors, 'active');
-        background-color: transparent;
-      }
-    }
-
-    &.btn-solid {
-      color: $white;
-      background-color: map-get($colors, 'default');
-      border: 1px solid map-get($colors, 'default');
-
-      &:hover {
+      @if $variant == 'solid' {
         color: $white;
-        background-color: map-get($colors, 'hover');
-        border: 1px solid map-get($colors, 'hover');
+        background-color: map-get(map-get(map-get($btnVariants, $variant), $color), 'default');
+
+        &:hover {
+          background-color: map-get(map-get(map-get($btnVariants, $variant), $color), 'hover');
+        }
+
+        &:active {
+          background-color: map-get(map-get(map-get($btnVariants, $variant), $color), 'active');
+        }
       }
+      @if $variant == 'ghost' {
+        color: map-get(map-get(map-get($btnVariants, $variant), $color), 'default');
 
-      &:active {
-        color: $white;
-        background-color: map-get($colors, 'active');
-        border: 1px solid map-get($colors, 'active');
+        &:hover {
+          background-color: map-get(map-get(map-get($btnVariants, $variant), $color), 'hover');
+        }
+
+        &:active {
+          background-color: map-get(map-get(map-get($btnVariants, $variant), $color), 'active');
+        }
       }
-    }
+      @if $variant == 'link' {
+        color: map-get(map-get(map-get($btnVariants, $variant), $color), 'default');
 
-    &.btn-link {
-      color: map-get($colors, 'default');
-      background-color: transparent;
-      border: none;
+        &:hover {
+          text-decoration: underline;
+        }
 
-      &:hover {
-        color: map-get($colors, 'hover');
-        background-color: transparent;
-        border: none;
-        text-decoration: underline;
-      }
-
-      &:active {
-        color: map-get($colors, 'active');
-        background-color: transparent;
-        border: none;
-        text-decoration: underline;
-      }
-    }
-
-    &.btn-outline {
-      color: map-get($colors, 'default');
-      background-color: transparent;
-      border: 1px solid map-get($colors, 'default');
-
-      &:hover {
-        background-color: rgba(map-get($colors, 'hover'), 0.1);
-      }
-
-      &:active {
-        background-color: map-get($colors, 'active');
+        &:active {
+          color: map-get(map-get(map-get($btnVariants, $variant), $color), 'active');
+        }
       }
     }
   }
